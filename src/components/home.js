a3f57dd399d7eb68b53620351861e71eb4641809
Frane92 <frane.drazenovic@gmail.com>
2018-09-15 13:14:50 +0200

    added project

---
+++ src/components/home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+//Components
+import Featured from './featured';
+import Subscriptions from './subscriptions'
+import Blocks from './blocks'
+import Poll from './poll'
+import Teams from './teams';
+
+const URL_HOME = 'http://localhost:3004/home';
+
+class Home extends Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            home: ''
+        }
+    }
+    componentDidMount(){
+        fetch(URL_HOME,{method:'GET'})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                home:json
+            })
+        })
+    }
+    render() {
+        return(
+            <div>
+                <Featured slides={this.state.home.slider}/>
+                <Subscriptions/>
+                <Blocks blocks={this.state.home.blocks}/>
+                <Poll />
+            </div>
+        )
+    }
+}
+export default Home;
